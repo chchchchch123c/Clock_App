d3aadcca0e865831eb992feb81092fac937b6bbd
LEGION <chchchchch123c@gmail.com>
2025-10-07 23:36:05 +0900

    2025-10-07

---
+++ lib/constant/color.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color kBackgroundColor = Color(0xFFF7F6FB);
+const Color kBlack = Color(0xFF121212);
+const Color kGray = Color(0xFF9A999E);
+const Color kWhite = Color(0xFFFCFBFF);
