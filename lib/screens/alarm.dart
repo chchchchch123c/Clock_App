763a205856289d68203e999b717c8b6eb3ebecce
LEGION <chchchchch123c@gmail.com>
2025-10-07 23:36:26 +0900

    2025-10-07

---
+++ lib/screens/alarm.dart
@@ -0,0 +1,13 @@
+import 'package:clock_app/screens/widgets/base_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class Alarm extends StatelessWidget {
+  const Alarm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScaffold(
+      body: Column(),
+    );
+  }
+}
