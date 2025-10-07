d43966edcceae94c0fe5ad5f905b706d012b7864
LEGION <chchchchch123c@gmail.com>
2025-10-07 23:36:08 +0900

    2025-10-07

---
+++ lib/screens/widgets/base_app_bar.dart
@@ -0,0 +1,27 @@
+import 'package:clock_app/constant/color.dart';
+import 'package:flutter/material.dart';
+
+class BaseAppBar extends StatelessWidget implements PreferredSizeWidget{
+  final VoidCallback onTap;
+  const BaseAppBar({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      toolbarHeight: 88,
+      backgroundColor: kBackgroundColor,
+      actionsPadding: EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      actions: [
+        GestureDetector(
+          onTap: onTap,
+          child: Icon(Icons.more_vert),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(88);
+}
