7085233f96eed6d4ab4004d5ca4c11244f9d5be8
LEGION <chchchchch123c@gmail.com>
2025-10-07 23:36:22 +0900

    2025-10-07

---
+++ lib/screens/main_tab.dart
@@ -0,0 +1,47 @@
+import 'package:clock_app/constant/color.dart';
+import 'package:clock_app/screens/alarm.dart';
+import 'package:clock_app/screens/world_clock.dart';
+import 'package:clock_app/screens/stopwatch.dart';
+import 'package:clock_app/screens/timer.dart';
+import 'package:flutter/material.dart';
+
+class MainTab extends StatelessWidget {
+  const MainTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            children: [
+              Alarm(),
+              WorldClock(),
+              Stopwatch(),
+              Timer(),
+            ],
+          ),
+          bottomNavigationBar: Container(
+            color: kBackgroundColor,
+            height: 56,
+            child: TabBar(
+              labelPadding: EdgeInsets.symmetric(
+                vertical: 18,
+              ),
+              indicatorColor: kBlack,
+              labelColor: kBlack,
+              unselectedLabelColor: kGray,
+              tabs: [
+                Text('알람'),
+                Text('세계시각'),
+                Text('스톱워치'),
+                Text('타이머'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
