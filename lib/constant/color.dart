25473cc293c0aa15452682d2bd21c15d60b0dc2e
LEGION <chchchchch123c@gmail.com>
2025-12-05 16:38:49 +0900

    2025-12-05

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -1,6 +1,21 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 const Color kBackgroundColor = Color(0xFFF7F6FB);
 const Color kBlack = Color(0xFF121212);
 const Color kGray = Color(0xFF9A999E);
 const Color kWhite = Color(0xFFFCFBFF);
+
+const LinearGradient kPointGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xFFE3465B),
+    Color(0xFFC017E4),
+    Color(0xFF8150FE),
+  ],
+  stops: [
+    0.0,
+    0.5,
+    1.0,
+  ],
+);
