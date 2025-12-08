b99e1eed7f2a4b90ea9850ca8752cbc1ef6c66d0
LEGION <chchchchch123c@gmail.com>
2025-12-08 16:03:18 +0900

    2025-12-08

---
+++ lib/utils/screen_size.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ScreenSize {
+
+  static double width(context) => MediaQuery.sizeOf(context).width;
+
+  static double height(context) => MediaQuery.sizeOf(context).height;
+
+}
