e9f3a56c38925fcf33c5b6a2d8fc4a17192b5d5e
LEGION <chchchchch123c@gmail.com>
2025-12-08 16:36:48 +0900

    2025-12-08

---
+++ lib/screens/components/4_timer/time_wheel_picker.dart
@@ -0,0 +1,52 @@
+import 'package:clock_app/constant/color.dart';
+import 'package:flutter/material.dart';
+
+class TimeWheelPicker extends StatelessWidget {
+  final ValueNotifier<int> selectedIndexNotifier;
+  final FixedExtentScrollController controller;
+  final int itemCount;
+
+  const TimeWheelPicker({
+    super.key,
+    required this.selectedIndexNotifier,
+    required this.controller,
+    this.itemCount = 100,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxHeight: 69 * 3,
+      ),
+      child: ValueListenableBuilder<int>(
+        valueListenable: selectedIndexNotifier,
+        builder: (context, selectedIndex, _) {
+          return ListWheelScrollView.useDelegate(
+            controller: controller,
+            itemExtent: 69,
+            onSelectedItemChanged: (index) {
+              selectedIndexNotifier.value = index;
+            },
+            childDelegate: ListWheelChildLoopingListDelegate(
+              children: List.generate(itemCount, (index) {
+                final isSelected = index == selectedIndex;
+                return Center(
+                  child: Text(
+                    '$index',
+                    style: TextStyle(
+                      fontSize: 48,
+                      color: isSelected ? kBlack : kGray,
+                      fontWeight:
+                      isSelected ? FontWeight.bold : FontWeight.normal,
+                    ),
+                  ),
+                );
+              }),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
