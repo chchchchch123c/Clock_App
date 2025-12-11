d70fb5816224589c5212400cd25b8cd4412393f8
LEGION <chchchchch123c@gmail.com>
2025-12-11 20:25:22 +0900

    2025-12-11

--- lib/controllers/4_timer_controller.dart
+++ lib/controllers/4_timer_controller.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class TimerController {
+  final TimerWheelState hours = TimerWheelState();
+  final TimerWheelState minutes = TimerWheelState();
+  final TimerWheelState seconds = TimerWheelState();
+}
 
+class TimerWheelState {
   final ValueNotifier<int> selectedIndex = ValueNotifier<int>(0);
-  final FixedExtentScrollController wheelController =
-  FixedExtentScrollController();
-
+  final ValueNotifier<bool> isDragging = ValueNotifier(false);
+  final FixedExtentScrollController controller = FixedExtentScrollController();
 }
