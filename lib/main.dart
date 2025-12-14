0e478cc365a0f8fec3290da5d2cb73454a1035fd
LEGION <chchchchch123c@gmail.com>
2025-12-14 22:48:17 +0900

    2025-12-14

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:clock_app/screens/main_tab.dart';
 import 'package:clock_app/theme/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    theme: DesignSystem.theme.themeData,
-    home: MainTab(),
-  ));
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(
+    MaterialApp(
+      theme: DesignSystem.theme.themeData,
+      home: MainTab(),
+    ),
+  );
 }
