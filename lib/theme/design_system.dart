bcf3631de870a335da9422df5200e9b5516b27ef
LEGION <chchchchch123c@gmail.com>
2025-12-18 01:35:00 +0900

    2025-12-18

--- lib/theme/design_system.dart
+++ lib/theme/design_system.dart
@@ -1,14 +1,12 @@
 import 'package:clock_app/theme/app_colors.dart';
 import 'package:clock_app/theme/app_font_family.dart';
 import 'package:clock_app/theme/app_font_size.dart';
-import 'package:clock_app/theme/app_theme.dart';
 
 class DesignSystem {
 
   static const color = AppColor();
   static const fontSize = FontSize();
   static const fontFamily = FontFamily();
-  static final theme = AppTheme();
 
 }
 
