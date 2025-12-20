05e0644520f095529bdcd68156fdfbce1b9db359
LEGION <chchchchch123c@gmail.com>
2025-12-20 23:03:22 +0900

    2025-12-18

--- lib/screens/components/4_timer/wheels/time_wheel_picker_row.dart
+++ lib/screens/components/4_timer/wheels/time_wheel_picker_row.dart
@@ -1,44 +1,62 @@
 import 'package:clock_app/controllers/4_timer_controller.dart';
-import 'package:clock_app/screens/components/4_timer/labels/time_separator.dart';
+import 'package:clock_app/screens/components/4_timer/labels/wheels/time_separator_label.dart';
+import 'package:clock_app/screens/components/4_timer/labels/wheels/time_unit_label_row.dart';
 import 'package:clock_app/screens/components/4_timer/wheels/time_wheel_picker.dart';
+import 'package:clock_app/utils/size_util.dart';
 import 'package:flutter/material.dart';
 
 class TimeWheelPickerRow extends StatelessWidget {
   final TimerController controller;
+  final bool isClick;
 
-  const TimeWheelPickerRow({super.key, required this.controller});
+  const TimeWheelPickerRow({super.key, required this.controller, required this.isClick});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Spacer(flex: 3),
-        TimeWheelPicker(
-          controller: controller.hours.controller,
-          selectedIndexNotifier: controller.hours.selectedIndex,
-          isDraggingNotifier: controller.hours.isDragging,
-          itemCount: 100,
+    final width = SizeUtil.width(context);
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 600),
+      opacity: isClick ? 0 : 1,
+      child: SizedBox(
+        width: isClick ? 0 : width,
+        height: isClick ? 0 : width * 0.8,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: 16,
+              child: TimeUnitLabelRow(),
+            ),
+            Row(
+              children: [
+                Spacer(flex: 3),
+                TimeWheelPicker(
+                  selectedIndexNotifier: controller.hours.selectedIndex,
+                  isDraggingNotifier: controller.hours.isDragging,
+                  itemCount: 100,
+                ),
+                Spacer(),
+                TimeSeparatorLabel(),
+                Spacer(),
+                TimeWheelPicker(
+                  selectedIndexNotifier: controller.minutes.selectedIndex,
+                  isDraggingNotifier: controller.minutes.isDragging,
+                  itemCount: 60,
+                ),
+                Spacer(),
+                TimeSeparatorLabel(),
+                Spacer(),
+                TimeWheelPicker(
+                  selectedIndexNotifier: controller.seconds.selectedIndex,
+                  isDraggingNotifier: controller.seconds.isDragging,
+                  itemCount: 60,
+                ),
+                Spacer(flex: 3),
+              ],
+            ),
+          ],
         ),
-        Spacer(),
-        TimeSeparator(),
-        Spacer(),
-        TimeWheelPicker(
-          controller: controller.minutes.controller,
-          selectedIndexNotifier: controller.minutes.selectedIndex,
-          isDraggingNotifier: controller.minutes.isDragging,
-          itemCount: 60,
-        ),
-        Spacer(),
-        TimeSeparator(),
-        Spacer(),
-        TimeWheelPicker(
-          controller: controller.seconds.controller,
-          selectedIndexNotifier: controller.seconds.selectedIndex,
-          isDraggingNotifier: controller.seconds.isDragging,
-          itemCount: 60,
-        ),
-        Spacer(flex: 3),
-      ],
+      ),
     );
   }
 }
