1a0dc8b4c0e0c48ec9c94d357d9b865aa614b4a1
LEGION <chchchchch123c@gmail.com>
2025-12-20 23:02:41 +0900

    2025-12-18

--- lib/theme/app_font_size.dart
+++ lib/theme/app_font_size.dart
@@ -1,13 +1,24 @@
 class FontSize {
   const FontSize();
 
+  /// 64
   final double displayLarge = 64;
+
+  /// 48
   final double displayMedium = 48;
+
+  /// 32
   final double displaySmall = 32;
 
-  final double bodyLarge = 16;
-  final double bodyMedium = 14;
-  final double bodySmall = 12;
-
+  /// 18
   final double labelLarge = 18;
+
+  /// 16
+  final double bodyLarge = 16;
+
+  /// 14
+  final double bodyMedium = 14;
+
+  /// 12
+  final double bodySmall = 12;
 }
