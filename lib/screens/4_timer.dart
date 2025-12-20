2779bcb8719f813cb34f795e847131f658342a80
LEGION <chchchchch123c@gmail.com>
2025-12-20 23:02:33 +0900

    2025-12-18

--- lib/screens/4_timer.dart
+++ lib/screens/4_timer.dart
@@ -1,53 +1,69 @@
 import 'package:clock_app/controllers/4_timer_controller.dart';
-import 'package:clock_app/screens/components/4_timer/buttons/timer_start_button.dart';
-import 'package:clock_app/screens/components/4_timer/labels/time_unit_label_row.dart';
+import 'package:clock_app/screens/components/4_timer/progress_Indicator/progress_indicator_column.dart';
 import 'package:clock_app/screens/components/4_timer/wheels/time_wheel_picker_row.dart';
 import 'package:clock_app/screens/widgets/base_app_bar.dart';
 import 'package:clock_app/screens/widgets/base_scaffold.dart';
-import 'package:clock_app/utils/size_util.dart';
 import 'package:flutter/material.dart';
 
-class Timer extends StatelessWidget {
+import 'components/4_timer/button/timer_start_button.dart';
+
+class Timer extends StatefulWidget {
   final TimerController controller;
 
   const Timer({super.key, required this.controller});
 
   @override
+  State<Timer> createState() => _TimerState();
+}
+
+class _TimerState extends State<Timer> with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.initAnimation(this);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.disposeAnimation();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final width = SizeUtil.width(context);
     return BaseScaffold(
       appBar: BaseAppBar(
         onTap: () {},
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Positioned(
-            top: 24,
-            child: SizedBox(
-              width: width,
-              height: width * 0.8,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned(
-                    top: 16,
-                    child: TimeUnitLabelRow(),
-                  ),
-                  TimeWheelPickerRow(
-                    controller: controller,
-                  ),
-                ],
+      body: ValueListenableBuilder<bool>(
+        valueListenable: widget.controller.isHideWheel,
+        builder: (context, isClick, _) {
+          return Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 24,
+                child: ProgressIndicatorColumn(
+                  controller: widget.controller,
+                  isClick: isClick,
+                ),
               ),
-            ),
-          ),
-          Positioned(
-            bottom: 64,
-            child: TimerStartButton(
-              controller: controller,
-            ),
-          ),
-        ],
+              Positioned(
+                top: 24,
+                child: TimeWheelPickerRow(
+                  controller: widget.controller,
+                  isClick: isClick,
+                ),
+              ),
+              Positioned(
+                bottom: 64,
+                child: TimerStartButton(
+                  controller: widget.controller,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
