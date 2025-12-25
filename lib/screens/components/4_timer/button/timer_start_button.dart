dbeb6ab36b4244084881c611fc898c213718e372
LEGION <chchchchch123c@gmail.com>
2025-12-26 02:15:06 +0900

    2025-12-26

--- lib/screens/components/4_timer/button/timer_start_button.dart
+++ lib/screens/components/4_timer/button/timer_start_button.dart
@@ -1,5 +1,5 @@
 import 'package:clock_app/controllers/4_timer_controller.dart';
-import 'package:clock_app/theme/design_system.dart';
+import 'package:clock_app/screens/widgets/base_button.dart';
 import 'package:flutter/material.dart';
 
 class TimerStartButton extends StatelessWidget {
@@ -8,34 +8,11 @@ class TimerStartButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          gradient: DesignSystem.color.kPointGradient,
-          borderRadius: BorderRadius.all(Radius.circular(56))
-      ),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-        ),
-        onPressed: () {
-          controller.startTimer();
-        },
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 28,
-            vertical: 12,
-          ),
-          child: Text(
-            '시작', //  TODO => '중지'로 바뀌어야 함
-            style: TextStyle(
-              color: DesignSystem.color.kWhite,
-              fontSize: DesignSystem.fontSize.labelLarge,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
-      ),
+    return BaseButton(
+      onPressed: () {
+        controller.startTimer();
+      },
+      text: '시작',
     );
   }
 }
